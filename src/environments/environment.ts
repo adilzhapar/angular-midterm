6cf39850baf06c24f296aef9a5f833380d31aa12
Adil Zhapar <adilzhapar2002@gmail.com>
2023-10-24 19:34:31 +0600

    fixed readme

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   supabaseUrl: 'https://fguwaoubljnqfbkvnhji.supabase.co',
   supabaseKey:
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZndXdhb3VibGpucWZia3ZuaGppIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU3NTE4NjMsImV4cCI6MjAxMTMyNzg2M30.q85X8WWtm2mlV1sNjrim3A4ckJUc6xHMJFYQGCaegeE',
